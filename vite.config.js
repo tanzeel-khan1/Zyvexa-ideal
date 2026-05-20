86dade0671079b3694d4b0230eb12f06f212513f
tanzeel-khan1 <tanzeel0680@gmail.com>
2026-05-20 13:24:24 +0500

    confige

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    babel({ presets: [reactCompilerPreset()] }),
+  ],
+
+  server: {
+    port: 5000,
+  },
+});                     
